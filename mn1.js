448bd66b3a676ba9e933affabba1ed9344bcdb81
X-60TP <101022789+X-60TP@users.noreply.github.com>
2023-12-10 07:29:40 +0800

    Update mn1.js

--- mn1.js
+++ mn1.js
@@ -2,7 +2,7 @@ $httpClient.get('https://api.my-ip.io/ip', function (error, response, data) {
   $done({
     title: '当前 IP 地址',
     content: data,
+    icon: https://raw.githubusercontent.com/fanmingming/Rules/main/Filter/Netflix.png,
     backgroundColor: '#663399',
-    icon: 'network',
-  })
+ })
 })
